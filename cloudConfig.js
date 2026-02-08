d7f358dcbac4ac194cce4ba37adacf9cbac56f8d
sumitra63050 <sumitrachoudhary63050@gmail.com>
2026-02-08 21:21:09 +0530

    fix cloudinary error

--- cloudConfig.js
+++ cloudConfig.js
@@ -1,9 +1,9 @@
 const cloudinary = require('cloudinary').v2;
-const { CloudinaryStorage } = require('multer-storage-cloudinary');
+const {CloudinaryStorage} = require('multer-storage-cloudinary');
 
 cloudinary.config({
     cloud_name: process.env.CLOUD_NAME,
-    api_key: process.env.CLOUD_API_KEY,
+    api_key: process.env.CLOUD_API_KEY,//first names are standard
     api_secret: process.env.CLOUD_API_SECRET,
 });
 
